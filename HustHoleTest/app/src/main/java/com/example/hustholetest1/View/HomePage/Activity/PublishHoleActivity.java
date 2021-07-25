af3a8e2123b641ac5581bbed7407e1b708fa4317
threefour-34 <1216526235@qq.com>
2021-07-25 20:52:10 +0800

    ok

--- HustHoleTest/app/src/main/java/com/example/hustholetest1/View/HomePage/Activity/PublishHoleActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.hustholetest1.View.HomePage.Activity;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class PublishHoleActivity extends AppCompatActivity {//发树洞
-
-
-
-}
---
+++ HustHoleTestZK/app/build/generated/data_binding_trigger/debug/com/example/hustholetest1/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.hustholetest1;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
