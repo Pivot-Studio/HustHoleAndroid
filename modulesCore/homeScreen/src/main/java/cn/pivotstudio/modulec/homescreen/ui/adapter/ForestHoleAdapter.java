972bf057224208e74647fe42cdbb32d4cd3c835c
maihao <mohuaihaoxm@163.com>
2022-06-04 02:11:46 +0800

    refactor: 写Adapter和databinding

--- modulesCore/homeScreen/src/main/java/cn/pivotstudio/modulec/homescreen/ui/adapter/ForestHoleAdapter.java
+++ modulesCore/homeScreen/src/main/java/cn/pivotstudio/modulec/homescreen/ui/adapter/ForestHoleAdapter.java
@@ -4,7 +4,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import cn.pivotstudio.modulec.homescreen.databinding.FragmentForestBinding;
+import cn.pivotstudio.modulec.homescreen.databinding.ItemForestBinding;
+import cn.pivotstudio.modulec.homescreen.model.ForestHoleResponse;
 
 
 /**
@@ -14,7 +22,34 @@ import androidx.recyclerview.widget.RecyclerView;
  * @version:1.0
  * @author: mhh
  */
-public class ForestHoleAdapter extends RecyclerView.Adapter<ForestHoleAdapter.ForestHoleViewHolder> {
+public class ForestHoleAdapter extends ListAdapter<ForestHoleResponse.ForestHole, ForestHoleAdapter.ForestHoleViewHolder> {
+
+    // DiffCallback 是一个饿汉式单例类，用法参照 ListAdapter 官方使用方法
+    public static class DiffCallback extends DiffUtil.ItemCallback<ForestHoleResponse.ForestHole> {
+
+        private static final DiffCallback DIFF_CALLBACK = new DiffCallback();
+
+        public static DiffCallback getInstance() {
+            return DIFF_CALLBACK;
+        }
+
+        private DiffCallback() { }
+
+        @Override
+        public boolean areItemsTheSame(@NonNull ForestHoleResponse.ForestHole oldItem, @NonNull ForestHoleResponse.ForestHole newItem) {
+            return oldItem.holeId.equals(newItem.holeId);
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull ForestHoleResponse.ForestHole oldItem, @NonNull ForestHoleResponse.ForestHole newItem) {
+            return oldItem.content.equals(newItem.content);
+        }
+    }
+
+    public ForestHoleAdapter() {
+        super(DiffCallback.getInstance());
+    }
+
 
     @NonNull
     @Override
@@ -27,15 +62,21 @@ public class ForestHoleAdapter extends RecyclerView.Adapter<ForestHoleAdapter.Fo
 
     }
 
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-
     static class ForestHoleViewHolder extends RecyclerView.ViewHolder {
-        public ForestHoleViewHolder(@NonNull View itemView) {
-            super(itemView);
+
+        private ItemForestBinding binding;
+
+        // Constructor
+        public ForestHoleViewHolder(ItemForestBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
+
+        void bind(ForestHoleResponse.ForestHole forestHole) {
+            binding.setForestHole(forestHole);
+        }
+
+
     }
 
 
