ce4be6cfb5ed903885c40cee475e91f9cff710ed
yuruop <91507827+921757623@users.noreply.github.com>
2022-10-12 11:52:04 +0800

    a small commit

--- modulesBase/libBase/src/main/java/cn/pivotstudio/moduleb/libbase/constant/Constant.kt
+++ modulesBase/libBase/src/main/java/cn/pivotstudio/moduleb/libbase/constant/Constant.kt
@@ -1,4 +1,4 @@
-package cn.pivotstudio.moduleb.libbase.constant;
+package cn.pivotstudio.moduleb.libbase.constant
 
 /**
  * @classname:Constant
@@ -7,67 +7,79 @@ package cn.pivotstudio.moduleb.libbase.constant;
  * @version:1.0
  * @author:
  */
-public class Constant {
+object Constant {
     /**
      * MMKV
      */
     //是否登录
-    public static final String IS_LOGIN = "isLogin";
+    const val IS_LOGIN = "isLogin"
+
     //登录获得的token
-    public static final String USER_TOKEN = "USER_TOKEN";
-    public static final String USER_TOKEN_V2 = "USER_TOKEN_V2";
+    const val USER_TOKEN = "USER_TOKEN"
+    const val USER_TOKEN_V2 = "USER_TOKEN_V2"
+
     //是否第一次使用app
-    public static final String IS_FIRST_USED = "isFirstUseD";
+    const val IS_FIRST_USED = "isFirstUseD"
+
     //发布数洞的内容
-    public static final String HOLE_TEXT = "HOLE_TEXT";
+    const val HOLE_TEXT = "HOLE_TEXT"
+
     //近期使用的表情list
-    public static final String UsedEmoji = "USED_EMOJI";
+    const val UsedEmoji = "USED_EMOJI"
+
     /**
      * ARouter
      */
     //树洞号的标识
-    public static final String HOLE_ID = "HOLE_ID";
+    const val HOLE_ID = "HOLE_ID"
+
     // 小树林id key
-    public static final String FOREST_ID = "FOREST_ID";
-    public static final String FOREST_NAME = "FOREST_NAME";
+    const val FOREST_ID = "FOREST_ID"
+    const val FOREST_NAME = "FOREST_NAME"
+
     // 是否从小树林跳转 key
-    public static final String FROM_DETAIL_FOREST = "FROM_DETAIL_FOREST";
+    const val FROM_DETAIL_FOREST = "FROM_DETAIL_FOREST"
+
     //选择号的标识
-    public static final String REPLY_ID = "reply_local_id";
+    const val REPLY_ID = "reply_local_id"
+
     //是否需要打开键盘的标识
-    public static final String IF_OPEN_KEYBOARD = "IF_OPEN_KEYBOARD";
+    const val IF_OPEN_KEYBOARD = "IF_OPEN_KEYBOARD"
+
     //内容的拥有者的标识
-    public static final String ALIAS = "alias";
+    const val ALIAS = "alias"
 
     /**
      * onActivityResult
      */
     //从树洞界面返回数据的标识
-    public static final String HOLE_RETURN_INFO = "holeReturnInfo";
-    public static final String HOLE_LIKED = "HOLE_LIKED";
-    public static final String HOLE_LIKE_COUNT = "HOLE_LIKE_COUNT";
-    public static final String HOLE_REPLIED = "HOLE_REPLIED";
-    public static final String HOLE_REPLY_COUNT = "HOLE_REPLY_COUNT";
-    public static final String HOLE_FOLLOWED = "HOLE_FOLLOWED";
-    public static final String HOLE_FOLLOW_COUNT = "HOLE_FOLLOW_COUNT";
+    const val HOLE_RETURN_INFO = "holeReturnInfo"
+    const val HOLE_LIKED = "HOLE_LIKED"
+    const val HOLE_LIKE_COUNT = "HOLE_LIKE_COUNT"
+    const val HOLE_REPLIED = "HOLE_REPLIED"
+    const val HOLE_REPLY_COUNT = "HOLE_REPLY_COUNT"
+    const val HOLE_FOLLOWED = "HOLE_FOLLOWED"
+    const val HOLE_FOLLOW_COUNT = "HOLE_FOLLOW_COUNT"
+
     /**
      * new Activity
      */
     //邮箱头，及学号
-    public static final String EMAIL_HEAD = "email_head";
-    //验证码内容
-    public static final String VERIFY_CODE = "verify_code";
+    const val EMAIL_HEAD = "email_head"
 
-    public static final String EMAIL_SUFFIX = "@hust.edu.cn";
+    //验证码内容
+    const val VERIFY_CODE = "verify_code"
+    const val EMAIL_SUFFIX = "@hust.edu.cn"
+
     /**
      * Base
      */
     //网络请求url头
-    public static final String BASE_URL = "https://husthole.pivotstudio.cn/api/";
+    const val BASE_URL = "https://husthole.pivotstudio.cn/api/"
+
     //每次加载的列表长度
-    public static final Integer CONSTANT_STANDARD_LOAD_SIZE = 20;
+    const val CONSTANT_STANDARD_LOAD_SIZE = 20
+
     //数据库
-    public static final String ROOM_DATABASE_NAME = "hust_hole";
-
-
+    const val ROOM_DATABASE_NAME = "hust_hole"
 }
